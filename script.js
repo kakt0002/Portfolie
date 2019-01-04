71fb0e1130a2dc4033c670bf4d6efc7885ead3aa
Kaktus Frederikke Høy Bendtsen <kaktusfrederikkehoybendtsen@Kaktuss-MacBook-Pro.local>
2019-01-04 13:28:56 +0100

    alle bokse virker samt burger menu korrekt nu

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+window.addEventListener("load", sidenVises);
 
+function sidenVises() {
+    console.log("siden vises!");
+    //registrer klik på menu-knap
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+}
+
+function toggleMenu() {
+    console.log("Toggle menu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+
+        //menuen er nu skjult - ændr menuknap til lll
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        //menuen er nu vist - ændr menuknap til x
+        document.querySelector("#menuknap").textContent = "✕";
+    }
+}
